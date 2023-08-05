64317930650e20bcda9268ba3193d2eb085ee391
王鹏飞 <571048989@qq.com>
2023-08-05 12:41:39 +0800

    banner广告宽度占满问题

--- android/src/main/kotlin/com/example/admob_plugin/AdmobMixBanner.kt
+++ android/src/main/kotlin/com/example/admob_plugin/AdmobMixBanner.kt
@@ -1,32 +1,44 @@
 package com.example.admob_plugin
 
 import android.content.Context
-import android.graphics.Color
-import android.text.TextUtils
+import android.util.DisplayMetrics
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.FrameLayout
-import android.widget.LinearLayout
 import com.google.android.gms.ads.*
 import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.platform.PlatformView
-import java.util.*
 
 
-class AdmobMixBanner(context: Context, messenger: BinaryMessenger, id: Int) :
+class AdmobMixBanner(
+    val context: Context,
+    messenger: BinaryMessenger,
+    id: Int,
+) :
     PlatformView {
 
     private var adViewContainer: FrameLayout? = null
 
+
     init {
         //这个xml一定不能复用
-        adViewContainer = LayoutInflater.from(context).inflate(R.layout.contain_layout, null, true) as FrameLayout?
+        adViewContainer = LayoutInflater.from(context)
+            .inflate(R.layout.contain_layout, null, false) as FrameLayout?
+
+        // 计算屏幕宽度
+        val displayMetrics: DisplayMetrics = context.resources.displayMetrics
+
+        val screenWidthPixels = displayMetrics.widthPixels
+        val density = displayMetrics.density
+        val screenWidthDp = (screenWidthPixels / density)
+        val adSize = AdSize(screenWidthDp.toInt(), 50)
+
         var adView = AdView(context)
         adView?.setAdUnitId("ca-app-pub-3940256099942544/6300978111")
         adViewContainer?.addView(adView)
         val adRequest = AdRequest.Builder().build()
-        adView?.setAdSize(AdSize.BANNER)
+        adView?.setAdSize(adSize)
         adView?.loadAd(adRequest)
         adView?.setAdListener(object : AdListener() {
             override fun onAdLoaded() {
@@ -67,5 +79,6 @@ class AdmobMixBanner(context: Context, messenger: BinaryMessenger, id: Int) :
     override fun dispose() {
 
     }
+
 
 }
